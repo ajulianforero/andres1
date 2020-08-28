77b8c10fb479579d341aab625331d1af7e7d81ea
andres <ajulianforero@ucundinamarca.edu.co>
2020-08-27 19:40:25 -0500

    primer cambio

---
+++ Reloj.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Reloj{
+public static void main(String[] args){	
+	for(int h=1; h <=23 ; h++){
+	for(int m=1; m <=59 ; m++){
+	for(int s=1; s <=59 ; s++){
+		System.out.println( + h + " : " + m + " ; " + s );
+		
+	}
+	
+}
+	}
+}
+}
+
